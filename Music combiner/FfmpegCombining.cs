770f74b650be4de1404bb50da13c2cf57d62d2ca
MrIvory124 <blueturtle124@gmail.com>
2023-10-20 21:47:12 +1300

    Outlined the specifics for this change

--- Music combiner/FfmpegCombining.cs
+++ Music combiner/FfmpegCombining.cs
@@ -10,72 +10,34 @@ namespace Music_combiner
 
         public string SongEncoding(List<string> songOrder, string outputDir) // take in the list of songs and combine them into a single file
         {
-
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("The output directory is " + outputDir);
-
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("Encoding begun");
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-
-            long milliseconds = DateTimeOffset.Now.ToUnixTimeMilliseconds(); //get a number that *cannot* be reproduced easily 
-            string workingDir = outputDir + @"\" + milliseconds.ToString() + "TempFolder"; //make a temporary folder with this random number in the name
-            Directory.CreateDirectory(workingDir);
-            System.Threading.Thread.Sleep(50);
-
-            string outputFileN;
-            for (int i1 = 1; i1 <= songOrder.Count; i1++) //TODO: tidy this up
-            {
-                if (i1 == 1) // on first run take the first 2 songs and combine them together
-                {
-                    string nInput1 = songOrder[0];
-                    string nInput2 = songOrder[1];
-                    outputFileN = workingDir + @"\output1.wav";
-                    _ = Ffmpeg(nInput1, nInput2, outputFileN); // calls ffmpeg method
-                    System.Threading.Thread.Sleep(100);
-                    Console.WriteLine("File output: " + outputFileN);
-                }
-                else if (i1 == songOrder.Count) // every other run combine the previous result with the next song
-                {
-                    string nInput1 = workingDir + @"\output" + (i1 - 2) + ".wav";
-                    string nInput2 = songOrder[(i1-1)];
-                    outputFileN = workingDir + @"\output" + i1 + ".wav";
-                    _ = Ffmpeg(nInput1, nInput2, outputFileN); // calls ffmpeg method
-                    System.Threading.Thread.Sleep(100);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Final file output: " + outputFileN);
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                }
-                else // upon final run output the path of the finished file into console
-                {
-                    string nInput1 = workingDir + @"\output" + (i1 - 1) + ".wav";
-                    string nInput2 = songOrder[i1];
-                    outputFileN = workingDir + @"\output" + i1 + ".wav";
-                    _ = Ffmpeg(nInput1, nInput2, outputFileN); // calls ffmpeg method
-                    System.Threading.Thread.Sleep(100);
-                    Console.WriteLine("File output: " + outputFileN);
-                }
-
-            }
-
-
+ 
             return "this doesnt work if nothing is here and i cbf fixing it";
-        }
-        public string Ffmpeg(string inputFile1, string inputFile2, string outputFile) // method for running ffmpeg with crossfade filter TODO: replace this as its O^n
-        {
-            Console.WriteLine("Starting ffmpeg");
-            int crossFadeLen = 10; // TODO: impliment user change crossfade length
-            string command = "/C ffmpeg -loglevel error -i \"" + inputFile1 + "\"  -i \"" + inputFile2 + "\"  -vn -filter_complex \"acrossfade=d=" + crossFadeLen + ":c1=tri:c2=tri\" " + outputFile; // call ffmpeg using cmd with previously sorted files
-            ProcessStartInfo cmdsi = new ProcessStartInfo("cmd.exe");
-            cmdsi.Arguments = command;
-            Process cmd = Process.Start(cmdsi);
-            cmd.WaitForExit();
 
-            Console.WriteLine(outputFile);
-            Console.WriteLine("Stopping ffmpeg");
-            return outputFile;
         }
+
+
+    
     }
 }
 
 
+/*
+ 
+ How we want to do it:
+
+1. convert all mp3 songs to wav in a temp folder
+
+2. construct filtergraph with crossfades for n songs
+
+3. construct final ffmpeg command
+    0. ffmpeg +
+    i. -i (list of all songs in the songOrder order)
+    ii. -filter_complex + constructed filtergraph
+    iii. + -map "[out]" (userOutDir)output.mp3
+
+4. move final file to chosen output dir and delete any temporary files
+ 
+ ffmpeg -i song1.mp3 -i song2.mp3 -i song3.mp3 -i song4.mp3 -filter_complex "[0:a][1:a]acrossfade=d=10:c1=tri:c2=tri[a1];[a1][2:a]acrossfade=d=10:c1=tri:c2=tri[a2];[a2][3:a]acrossfade=d=10:c1=tri:c2=tri[out]" -map "[out]" output.mp3 // does actually work
+
+ 
+ */
